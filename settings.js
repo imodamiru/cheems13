84d87006ddbf0f546ae5b0d66ff36e972572da20
TeamGodLike2 <devilfakemail@gmail.com>
2024-06-01 09:18:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 //base by DGXeon
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
+//YouTube: @xenonbtz
+//Instagram: xenon_btz
 //Telegram: t.me/Haryanvi_69
-//GitHub: @DGXeon
+//GitHub: @TeamGodLike
 //WhatsApp: +447520651745
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
@@ -11,24 +11,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-al.socialm = "GitHub: DGXeon" //ur github or insta name
+global.ytname = "YT: XenonBtz" //ur yt chanel name
+al.socialm = "GitHub: TeamGodLike" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V13' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/N3kyujoG3vk"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.ownernumber = '447520651745' //ur owner number
+global.ownername = 'Xenon' //ur owner name
+global.websitex = "https://youtu.be/"
+global.wagc = "https://api.whatsapp.com/send?phone=+447520651745"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD13' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.botscript = 'https://github.com/TeamGodLike2/cheems13' //script link
+global.packname = "Sticker By Xenon"
+global.author = "Xeon\n\n+447520651745"
+global.creator = "447520651745@s.whatsapp.net"
+global.xprefix = '$'
+global.premium = ["447520651745"] // Premium User
 global.hituet = 0
 
 //bot sett
